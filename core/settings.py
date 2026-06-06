364659ef426ae32e6d8cd7ad61c8a2ab7fb240b6
Nandini Rathore <rathorepragati40@gmail.com>
2026-06-06 14:03:20 +0530

    Cleaned up pycache and updated gitignore

--- core/settings.py
+++ core/settings.py
@@ -19,6 +19,7 @@ INSTALLED_APPS = [
     'corsheaders',  # <--- Ye line add kar
     'rest_framework',
     'crm',
+    
 ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -29,6 +30,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -74,10 +76,13 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:5174",
+    "http://127.0.0.1:5174",
 ]
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
+CORS_ALLOW_CREDENTIALS = True
 STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
